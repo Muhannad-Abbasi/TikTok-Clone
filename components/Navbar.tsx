99e85d71c3522e8928f54ccb822f5d6868681c13
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2022-07-29 13:08:05 +0200

    ---

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import Image from 'next/image';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <Link>
+        <div>
+          <Image 
+            className='cursor-pointer'
+          />
+        </div>
+      </Link>
+    </div>
   )
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
 
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
@@ -15,10 +17,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   
   return (
     <div>
-      NavBar
+      <Navbar />
       <div className='flex gap-6 md:gap-20'>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-          SideBar
+          <Sidebar />
         </div>
         <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
           <Component {...pageProps} />
