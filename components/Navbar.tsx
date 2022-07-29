08bc70cb69104180cf36b71440628cf7e133ee68
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2022-07-29 14:40:05 +0200

    Sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,14 +6,18 @@ import Image from 'next/image';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
+import Logo from '../utils/tiktik-logo.png';
 
 const Navbar = () => {
   return (
-    <div>
-      <Link>
-        <div>
+    <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+      <Link href='/'>
+        <div className='w-[100px] md:w-[130px]'>
           <Image 
             className='cursor-pointer'
+            src={Logo}
+            alt='TikTok'
+            layout='responsive'
           />
         </div>
       </Link>
