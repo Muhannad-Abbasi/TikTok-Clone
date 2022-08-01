4ca01c5aab5345cd9bc9e981bafb7f27fb4fe87d
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2022-08-01 18:43:11 +0200

    Adding, editing and deleting...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import axios from 'axios';
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
+import VideoCard from '../components/VideoCard';
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  )
+interface IProps {
+  videos: Video[]
 }
 
-export default Home
+const Home = ({ videos }: IProps ) => {
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length ? (
+        videos.map((video: Video) => (
+          <VideoCard post={video} key={video._id} />
+        ))
+      ) : (
+        <NoResults text={'No Videos'} />
+      )}
+    </div>
+  )
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data
+    }
+  }
+}
+
+export default Home;
