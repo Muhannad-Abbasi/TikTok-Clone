73acd73794b857127ccb6c057aea34d218dd587c
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2022-08-03 20:01:13 +0200

    Adding google sign in and store it

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import Image from 'next/image';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils';
+import useAuthStore from '../store/authStore';
 
 const Navbar = () => {
+  const { userProfile, addUser } = useAuthStore();
+
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
       <Link href='/'>
@@ -21,6 +25,23 @@ const Navbar = () => {
           />
         </div>
       </Link>
+
+      <div>Search</div>
+
+      <div>
+        {userProfile ? (
+          <div className='flex gap-5 md:gap-10'>
+            <Link href='/'>
+              <button></button>
+            </Link>
+          </div>
+        ) : (
+          <GoogleLogin 
+            onSuccess={(response) => createOrGetUser(response, addUser)}
+            onError={() => console.log('Error')}
+          />
+        )}
+      </div>
     </div>
   )
 }
